e2144ffad2061a041d022f92138f0a49ba73ba38
Dan <dan84johansson@hotmail.com>
2023-05-11 09:23:58 +0200

    Receiving/reading same character multiple times

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -27,7 +27,6 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32l4xx_hal.h"
 #include "stm32l4xx_ll_crs.h"
 #include "stm32l4xx_ll_rcc.h"
 #include "stm32l4xx_ll_bus.h"
@@ -40,9 +39,13 @@ extern "C" {
 #include "stm32l4xx_ll_usart.h"
 #include "stm32l4xx_ll_gpio.h"
 
+#if defined(USE_FULL_ASSERT)
+#include "stm32_assert.h"
+#endif /* USE_FULL_ASSERT */
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stdint.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
@@ -68,23 +71,56 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define OSC32_IN_Pin LL_GPIO_PIN_14
-#define OSC32_IN_GPIO_Port GPIOC
-#define OSC32_OUT_Pin LL_GPIO_PIN_15
-#define OSC32_OUT_GPIO_Port GPIOC
-#define RS485_TX_Pin LL_GPIO_PIN_9
-#define RS485_TX_GPIO_Port GPIOA
-#define RS485_RX_Pin LL_GPIO_PIN_10
-#define RS485_RX_GPIO_Port GPIOA
-#define RS485_DE_Pin LL_GPIO_PIN_12
-#define RS485_DE_GPIO_Port GPIOA
-#define SWDIO_Pin LL_GPIO_PIN_13
-#define SWDIO_GPIO_Port GPIOA
-#define SWCLK_Pin LL_GPIO_PIN_14
-#define SWCLK_GPIO_Port GPIOA
-#define DBG_LED_Pin LL_GPIO_PIN_3
-#define DBG_LED_GPIO_Port GPIOB
+#define OSC32_IN_Pin 				LL_GPIO_PIN_14
+#define OSC32_IN_GPIO_Port 			GPIOC
+#define OSC32_OUT_Pin 				LL_GPIO_PIN_15
+#define OSC32_OUT_GPIO_Port 		GPIOC
+#define RS485_TX_Pin 				LL_GPIO_PIN_9
+#define RS485_TX_GPIO_Port 			GPIOA
+#define RS485_RX_Pin 				LL_GPIO_PIN_10
+#define RS485_RX_GPIO_Port 			GPIOA
+#define RS485_TXDE_Pin 				LL_GPIO_PIN_12
+#define RS485_TXDE_GPIO_Port 		GPIOA
+#define SWDIO_Pin 					LL_GPIO_PIN_13
+#define SWDIO_GPIO_Port 			GPIOA
+#define SWCLK_Pin 					LL_GPIO_PIN_14
+#define SWCLK_GPIO_Port 			GPIOA
+#define DBG_LED_Pin 				LL_GPIO_PIN_3
+#define DBG_LED_GPIO_Port 			GPIOB
+
+#ifndef NVIC_PRIORITYGROUP_0
+#define NVIC_PRIORITYGROUP_0         ((uint32_t)0x00000007) /*!< 0 bit  for pre-emption priority,
+                                                                 4 bits for subpriority */
+#define NVIC_PRIORITYGROUP_1         ((uint32_t)0x00000006) /*!< 1 bit  for pre-emption priority,
+                                                                 3 bits for subpriority */
+#define NVIC_PRIORITYGROUP_2         ((uint32_t)0x00000005) /*!< 2 bits for pre-emption priority,
+                                                                 2 bits for subpriority */
+#define NVIC_PRIORITYGROUP_3         ((uint32_t)0x00000004) /*!< 3 bits for pre-emption priority,
+                                                                 1 bit  for subpriority */
+#define NVIC_PRIORITYGROUP_4         ((uint32_t)0x00000003) /*!< 4 bits for pre-emption priority,
+                                                                 0 bit  for subpriority */
+#endif
 /* USER CODE BEGIN Private defines */
+
+#define RS485_GPIO_Port 		GPIOA
+
+#define DBG_LED_On()			LL_GPIO_SetOutputPin(DBG_LED_GPIO_Port, DBG_LED_Pin);
+#define DBG_LED_Off()			LL_GPIO_ResetOutputPin(DBG_LED_GPIO_Port, DBG_LED_Pin);
+#define DBG_LED_Toggle()		LL_GPIO_TogglePin(DBG_LED_GPIO_Port, DBG_LED_Pin);
+
+#define DBG_LED_Init()			DBG_LED_Off(); \
+								GPIO_InitStruct.Pin = DBG_LED_Pin; \
+								GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT; \
+								GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW; \
+								GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL; \
+								GPIO_InitStruct.Pull = LL_GPIO_PULL_NO; \
+								LL_GPIO_Init(DBG_LED_GPIO_Port, &GPIO_InitStruct);
+
+#define RS485_TxEnable() 		LL_GPIO_SetOutputPin(RS485_TXDE_GPIO_Port, RS485_TXDE_Pin);
+#define RS485_IsTxEnable()		(LL_GPIO_IsOutputPinSet(RS485_TXDE_GPIO_Port, RS485_TXDE_Pin) == 1u)
+#define RS485_TxDisable()		LL_GPIO_ResetOutputPin(RS485_TXDE_GPIO_Port, RS485_TXDE_Pin);
+
+
 
 /* USER CODE END Private defines */
 
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,27 +19,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -48,11 +27,7 @@
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
 /* USER CODE END 0 */
@@ -63,18 +38,12 @@ static void MX_USART1_UART_Init(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
+  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
   /* Configure the system clock */
   SystemClock_Config();
@@ -86,15 +55,62 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART1_UART_Init();
+
   /* USER CODE BEGIN 2 */
+
+  const char payload[8] = "Kamin\r\n";
+  //const uint8_t payload[8] = {0x4b, 0x61, 0x6d, 0x69, 0x6e, 0x0D, 0x0A};
+  uint8_t i = 0;
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  RS485_TxEnable();
   while (1)
   {
-    /* USER CODE END WHILE */
+
+      /* USER CODE END WHILE */
+	  LL_USART_TransmitData8(USART1, (uint8_t)payload[i++]);
+	  while(!LL_USART_IsActiveFlag_TXE(USART1)); // When the transmit shift-register has transmitted data in TDR to TX pin the TXE flag is set by HW.
+	  if(i==8){
+		  i= 0;
+	  }
+
+	  // LL_USART_IsActiveFlag_IDLE // Check if the USART IDLE line detected Flag is set or not.
+
+	  // LL_USART_EnableIT_RXNE  // CR1 RXNEIE LL_USART_EnableIT_RXNE RXNEIE - RX Not Empty Interrupt Enabled
+	  // LL_USART_IsEnabledIT_RXNE // CR1 RXNEIE LL_USART_IsEnabledIT_RXNE
+	  // LL_USART_DisableIT_RXNE // CR1 RXNEIE LL_USART_DisableIT_RXNE
+	  /*if(LL_USART_IsActiveFlag_RXNE(USART1)) { //Check if the USART Read Data Register Not Empty Flag is set or not.
+		  uint8_t index = 0;
+		  while ((uint8_t)(USART1->RDR)){
+			  payload[index] = (uint8_t)LL_USART_ReceiveData8(USART1);
+			  LL_USART_RequestRxDataFlush(USART1);
+			  index++;
+			  if(index > 254){
+				  DBG_LED_Toggle();
+				  break;
+			  }
+		  }
+		  LL_USART_RequestRxDataFlush(USART1);
+
+	  }*/
+	  // LL_USART_EnableIT_TXE // Enable TX Empty Interrupt. CR1 TXEIE LL_USART_EnableIT_TXE
+	  // LL_USART_DisableIT_TXE // CR1 TXEIE LL_USART_DisableIT_TXE
+	  // LL_USART_IsActiveFlag_TC //Check if the USART Transmission Complete Flag is set or not. ISR TC LL_USART_IsActiveFlag_TC
+	  // LL_USART_IsActiveFlag_TXE //Check if the USART Transmit Data Register Empty Flag is set or not. ISR TXE LL_USART_IsActiveFlag_TXE
+
+	  //LL_USART_IsActiveFlag_PE //Check if the USART Parity Error Flag is set or not. ISR PE LL_USART_IsActiveFlag_PE
+	  //LL_USART_IsActiveFlag_FE //Check if the USART Framing Error Flag is set or not. ISR FE LL_USART_IsActiveFlag_FE
+	  // LL_USART_IsActiveFlag_NE //Check if the USART Noise error detected Flag is set or not. ISR NF LL_USART_IsActiveFlag_NE
+	  // LL_USART_IsActiveFlag_ORE // Check if the USART OverRun Error Flag is set or not. ISR ORE LL_USART_IsActiveFlag_ORE
+
+	  // Nothing has gotten loaded. The output buffer should be lowered
+	  			// So that we are prepared for reception.
+	  			// Enable the transmit complete interrupt and disable it there instead
+	  			//LL_USART_ClearFlag_TC(USARTSettings[id].pxUSART);
+	  			//LL_USART_EnableIT_TC(USARTSettings[id].pxUSART);
 
     /* USER CODE BEGIN 3 */
   }
@@ -142,13 +158,10 @@ void SystemClock_Config(void)
   LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
   LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
   LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
-  LL_SetSystemCoreClock(4000000);
 
-   /* Update the time base */
-  if (HAL_InitTick (TICK_INT_PRIORITY) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  LL_Init1msTick(4000000);
+
+  LL_SetSystemCoreClock(4000000);
 }
 
 /**
@@ -161,52 +174,89 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE BEGIN USART1_Init 0 */
 
+	LL_GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+
+	// Peripheral clock enable //
+	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_USART1);
+	LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+	// USART1 GPIO Configuration
+	// PA9   ------>  RS485_TX
+	// PA10  ------>  RS485_RX
+	// PA12  ------>  RS485_TXDE
+
+	GPIO_InitStruct.Pin = RS485_TX_Pin | RS485_RX_Pin;
+	GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+	GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
+	LL_GPIO_Init(RS485_GPIO_Port, &GPIO_InitStruct);
+
+	LL_RCC_SetUSARTClockSource(LL_RCC_USART1_CLKSOURCE_SYSCLK);
+
+	// TX/RX direction
+	LL_USART_SetTransferDirection(USART1, LL_USART_DIRECTION_TX_RX);
+
+	// 8 data bit, 1 start bit, 1 stop bit, no parity
+	// Set the PS, PCE, M1, and M2 bits in USART_CR1 register and the STOP bit in USART_CR2 register
+	LL_USART_ConfigCharacter(USART1, LL_USART_DATAWIDTH_8B, LL_USART_PARITY_NONE, LL_USART_STOPBITS_1); // Not needed since default values
+
+	// Configure transfer bit order : MSB first
+	LL_USART_SetTransferBitOrder(USART1, LL_USART_BITORDER_LSBFIRST); // Not needed
+
+	// Configure Clock signal format as: Phase 2 edges, Polarity Low, Last Bit Clock output enabled
+	// Sets the CPHA [9], CPOL [10], and LBCL [8] bits in USART_CR2 register.
+	LL_USART_ConfigClock(USART1, LL_USART_PHASE_2EDGE, LL_USART_POLARITY_LOW, LL_USART_LASTCLKPULSE_OUTPUT);
+
+	// Configure USART BRR register for achieving expected Baud Rate value.
+	const uint32_t oversampling_mode = LL_USART_OVERSAMPLING_16; // Define
+	uint32_t periph_clk = LL_RCC_GetUSARTClockFreq(LL_RCC_USART1_CLKSOURCE);
+	const uint32_t baudrate = 115200;
+	// Compute and set USARTDIV value in BRR Register (full BRR content) according to used Peripheral Clock, Oversampling
+	// mode, and expected Baud Rate values
+	LL_USART_SetBaudRate(USART1, periph_clk, oversampling_mode, baudrate);
+
+	// Enable Driver Enable (DE) mode.
+	// Instead of toggling the DEM bit in the USART_CR3 register (LL_USART_DisableDEMode and LL_USART_EnableDEMode),
+	// the RS485 driver will enabled/disabled by the defined commands RS485_TxEnable() and RS485_TxDisable().
+	// ... FIXME, can the LL_USART_EnableDEMode() command below be removed?
+	LL_USART_EnableDEMode(USART1);
+
+	// Select Driver Enable Polarity. Setting the DEP bit in the USART_CR3 register.
+	LL_USART_SetDESignalPolarity(USART1, LL_USART_DE_POLARITY_HIGH);
+
+	// Set DEAT (Driver Enable Assertion Time) bits in USART_CR1 register. Time value expressed on 5 bits ([4:0] bits).
+	// This 5-bit value defines the time between the activation of the DE (Driver Enable) signal and
+	// the beginning of the start bit.
+	// ... FIXME, can this be removed since LL_USART_DisableDEMode and LL_USART_EnableDEMode aren't used?
+	LL_USART_SetDEAssertionTime(USART1, 31u); // 31 = 0001 1111
+
+	// Set DEDT (Driver Enable De-assertion Time) bits in USART_CR1 register. This 5-bit value defines the time between
+	// the end of the last stop bit, in a transmitted message, and the de-activation of the DE (Driver Enable) signal.
+	// ... FIXME, can this be removed since LL_USART_DisableDEMode and LL_USART_EnableDEMode aren't used?
+	LL_USART_SetDEDeassertionTime(USART1, 31u);
+
+	//
+	LL_USART_Enable(USART1);
+
+	// Polling USART initialization
+	uint16_t nbrloops = 0;
+	while (1u) {
+		uint32_t teack = LL_USART_IsActiveFlag_TEACK(USART1); // Check if the USART Transmit Enable Acknowledge Flag is set or not.
+		uint32_t reack = LL_USART_IsActiveFlag_REACK(USART1); // Check if the USART Receive Enable Acknowledge Flag is set or not.
+		nbrloops++;
+		if ((teack == 0u) || (reack == 0u)) {
+
+		} else {
+			// Both are NOT zero
+			break;
+		}
+		if (nbrloops > 10000) {
+			DBG_LED_On(); // Debug LED lit up to inform that something is wrong.
+			//_NOP();
+		}
+	}
   /* USER CODE END USART1_Init 0 */
-
-  LL_USART_InitTypeDef USART_InitStruct = {0};
-
-  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  LL_RCC_SetUSARTClockSource(LL_RCC_USART1_CLKSOURCE_PCLK2);
-
-  /* Peripheral clock enable */
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_USART1);
-
-  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
-  /**USART1 GPIO Configuration
-  PA9   ------> USART1_TX
-  PA10   ------> USART1_RX
-  PA12   ------> USART1_DE
-  */
-  GPIO_InitStruct.Pin = RS485_TX_Pin|RS485_RX_Pin|RS485_DE_Pin;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
-  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
-  USART_InitStruct.BaudRate = 115200;
-  USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
-  USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
-  USART_InitStruct.Parity = LL_USART_PARITY_NONE;
-  USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
-  USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
-  USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
-  LL_USART_Init(USART1, &USART_InitStruct);
-  LL_USART_EnableDEMode(USART1);
-  LL_USART_SetDESignalPolarity(USART1, LL_USART_DE_POLARITY_HIGH);
-  LL_USART_SetDEAssertionTime(USART1, 0);
-  LL_USART_SetDEDeassertionTime(USART1, 0);
-  LL_USART_ConfigAsyncMode(USART1);
-  LL_USART_Enable(USART1);
-  /* USER CODE BEGIN USART1_Init 2 */
-
-  /* USER CODE END USART1_Init 2 */
-
 }
 
 /**
@@ -223,10 +273,17 @@ static void MX_GPIO_Init(void)
   LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
   LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
 
-  /**/
-  LL_GPIO_ResetOutputPin(DBG_LED_GPIO_Port, DBG_LED_Pin);
+  // Initialize TXDE pin
+  RS485_TxDisable();
+  GPIO_InitStruct.Pin = RS485_TXDE_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(RS485_TXDE_GPIO_Port, &GPIO_InitStruct);
 
-  /**/
+  // Initialize DEBUG LED
+  DBG_LED_Off();
   GPIO_InitStruct.Pin = DBG_LED_Pin;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
@@ -239,27 +296,6 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
-
-/**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM6 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  /* USER CODE BEGIN Callback 0 */
-
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM6) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
-
-  /* USER CODE END Callback 1 */
-}
 
 /**
   * @brief  This function is executed in case of error occurrence.
